e93ee6605f68a66d1cfc06802468729b9cf818c6
Your Name <you@example.com>
2018-06-05 23:55:52 +0300

    - Fixed typo in configuration

--- app/config.php
+++ app/config.php
@@ -2,7 +2,7 @@
 
 
 defined("DB_HOST") ? null : define("DB_HOST", "localhost");
-defined("DB_USER") ? null : define("DB_USER", "roo");
+defined("DB_USER") ? null : define("DB_USER", "root");
 defined("DB_PASS") ? null : define("DB_PASS", "");
 defined("DB_NAME") ? null : define("DB_NAME", "sms_php_DB");
 
