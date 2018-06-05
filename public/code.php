b07a2836e2722e2762c84c3b0d223276ba28af52
Your Name <you@example.com>
2018-06-06 00:25:30 +0300

    - Added error handling for code.php file

--- public/code.php
+++ public/code.php
@@ -4,26 +4,23 @@ use Twilio\Rest\Client;
 
 require '../includes/header.php';
 
-
-$client = new Client($config['account_sid'], $config['auth_token']);
-
-
-$_SESSION['code'] = $code = uniqid();
-$con->query("INSERT INTO verifications(code) VALUES('$code')");
-
 try {
-    $client->account->messages->create('+' . $config['code_number'], ['from' => $config['phone_number'], 'body' => $code]);
+    $client = new Client($config['account_sid'], $config['auth_token']);
 
 
-} catch (Services_Twilio_RestException $e) {
-
-    die('Sorry could not connect because it could not ' . $e->getMessage());
-
-
+    $_SESSION['code'] = $code = uniqid();
+    $con->query("INSERT INTO verifications(code) VALUES('$code')");
+    try {
+        $client->account->messages->create('+' . $config['code_number'], ['from' => $config['phone_number'], 'body' => $code]);
+        
+    } catch (Services_Twilio_RestException $e) {
+        die('Sorry could not connect because it could not ' . $e->getMessage());
+    }
+    echo "<h3 class='text-center bg-success'>You Code has been sent</h3>";
+} //catch exception
+catch (Exception $e) {
+    echo 'Message: ' . $e->getMessage();
 }
-
-
-echo "<h3 class='text-center bg-success'>You Code has been sent</h3>";
 
 
 ?>
